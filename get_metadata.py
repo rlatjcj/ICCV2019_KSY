87c1a807e241a1e553a6b4221f60c2e93fe0d25c
bbchip0103 <bbchip13@gmail.com>
2019-07-25 16:39:12 +0900

    Update get_metadata, get_segmentation_data

--- get_metadata.py
+++ get_metadata.py
@@ -1,17 +1,24 @@
+# pip install requests bs4 lxml wget
 import requests
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
 from functools import partial
 import os
-import os.path as path
+import os.path as pth
 import wget
 
-def get_metadata(url, path='./'):
-    # filename = url.split('/')[-1]
+def get_metadata(url, base_path='./'):
+    filename = url.split('/')[-1]
+    full_filename = pth.join(base_path, filename)
+    if pth.exists(full_filename):
+        return full_filename, 1
+    wget.download(url, out=base_path)
+    ### If you can't use wget, you can use below blocked code
+    ### But It's much slower than wget...
     # data = requests.get(url).data
-    # with open(path.join(path, filename), 'wb') as f:
+    # with open(full_filename, 'wb') as f:
     #     f.write(data)
-    wget.download(url, out=path)
+    return full_filename, 0
 
 if __name__=='__main__':
     target_url = 'https://storage.googleapis.com/openimages/web/download.html'
@@ -29,13 +36,20 @@ if __name__=='__main__':
     for row in rows:
         sub_name = row.select_one('div.col-2.titlecol').get_text().strip()
         if sub_name:
-            sub_path = path.join(base_path, sub_name)
+            sub_path = pth.join(base_path, sub_name)
             os.makedirs(sub_path, exist_ok=True)
             hrefs = [a.get('href') for a in row.select('a') if a.get('href')]
             hrefs = [href for href in hrefs 
                         if href.endswith('.csv') or href.endswith('.txt')]
             if hrefs:
-                download_func = partial(get_metadata, path=sub_path)
-                with Pool(8) as pool:
-                    pool.map(download_func, hrefs)
-    
+                download_func = partial(get_metadata, base_path=sub_path)
+                pool = Pool(8)
+                for filename, status in pool.imap_unordered(download_func, hrefs):
+                    if status == 0:
+                        print(filename + ' is saved.')
+                    elif status == 1:
+                        print(filename + ' is already exist.')
+                    else:
+                        print('???')
+                pool.close()
+                pool.join()
--- get_segmentation_data.py
+++ get_segmentation_data.py
@@ -3,15 +3,22 @@ from bs4 import BeautifulSoup
 from multiprocessing import Pool
 from functools import partial
 import os
-import os.path as path
+import os.path as pth
 import wget
+from zipfile import ZipFile
 
-def get_metadata(url, path='./'):
-    # filename = url.split('/')[-1]
+def get_metadata(url, base_path='./'):
+    filename = url.split('/')[-1]
+    full_filename = pth.join(base_path, filename)
+    if pth.exists(full_filename):
+        return full_filename, 1
+    wget.download(url, out=base_path)
+    ### If you can't use wget, you can use below blocked code
+    ### But It's much slower than wget...
     # data = requests.get(url).data
-    # with open(path.join(path, filename), 'wb') as f:
+    # with open(full_filename, 'wb') as f:
     #     f.write(data)
-    wget.download(url, out=path)
+    return full_filename, 0
 
 if __name__=='__main__':
     target_url = 'https://storage.googleapis.com/openimages/web/download.html'
@@ -23,16 +30,35 @@ if __name__=='__main__':
     rows = [row for row in rows 
                 if row.select_one('div.col-10') and row.select_one('div.col-2.titlecol')]
 
-    base_path = path.join('metadata', 'Segmentations')
+    base_path = pth.join('metadata', 'Segmentations')
     os.makedirs(base_path, exist_ok=True)
     base_url = 'https://storage.googleapis.com/openimages/v5/{}-masks/{}-masks-{}.zip'
 
     for sub_name in ['train', 'validation', 'test']:
-        sub_path = path.join(base_path, sub_name)
+        sub_path = pth.join(base_path, sub_name)
         os.makedirs(sub_path, exist_ok=True)
         urls = [base_url.format(sub_name, sub_name, offset) 
                 for offset in list(range(10))+['a','b','c','d','e','f']]
-        download_func = partial(get_metadata, path=sub_path)
-        with Pool(8) as pool:
-            pool.map(download_func, urls)
+        download_func = partial(get_metadata, base_path=sub_path)
+        pool = Pool(8)
+        for filename, status in pool.imap_unordered(download_func, urls):
+            if status == 0:
+                print(filename + ' is saved.')
+            elif status == 1:
+                print(filename + ' is already exist.')
+            else:
+                print('???')
+        pool.close()
+        pool.join()
     
+    for sub_name in ['train', 'validation', 'test']:
+        sub_path = pth.join(base_path, sub_name)
+        zip_filename_list = [filename for filename in os.listdir(sub_path) if filename.endswith('.zip')]
+        for filename in zip_filename_list:
+            segment_zip_filename = pth.join(sub_path, filename)
+            with ZipFile(segment_zip_filename, 'r') as zip_ref:
+                zip_ref.extractall(sub_path)
+            os.remove(segment_zip_filename)
+            print(segment_zip_filename+ ' was extracted')
+
+        
