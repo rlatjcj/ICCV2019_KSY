6e68499a970b821ce121b3d73cbd77fef8bb533e
rlatjcj <rlatjcj@naver.com>
2019-08-12 00:42:27 +0900

    [190811] Complete baseline

--- loss.py
+++ loss.py
@@ -8,78 +8,67 @@ from utils import one_hot_embedding
 from torch.autograd import Variable
 
 
-class FocalLoss(nn.Module):
+class TotalLoss(nn.Module):
     def __init__(self, gamma=2., alpha=.25, classes=599):
-        super(FocalLoss, self).__init__()
+        super(TotalLoss, self).__init__()
         self.classes = classes
-        self.gamma = gamma
-        self.alpha = alpha
+
+    def _one_hot_embeding(self, labels):
+        """Embeding labels to one-hot form.
+        Args:
+            labels(LongTensor): class labels
+            classes(int): number of classes
+        Returns:
+            encoded labels, sized[N, #classes]
+        """
+
+        y = torch.eye(self.classes+1)  # [D, D]
+        return y[labels]  # [N, D]
 
     def focal_loss(self, x, y):
-        '''Focal loss.
-
+        """Focal loss
         Args:
-          x: (tensor) sized [N,D].
-          y: (tensor) sized [N,].
+            x(tensor): size [N, D]
+            y(tensor): size [N, ]
+        Returns:
+            (tensor): focal loss
+        """
 
-        Return:
-          (tensor) focal loss.
-        '''
-        t = one_hot_embedding(y.data.cpu(), 1+self.classes)
-        print(t.size())
-        t = t[:,1:]  # exclude background
-        print(t.size())
+        alpha = 0.25
+        gamma = 2
+
+        t = self._one_hot_embeding(y.data.cpu())  # [N,21]
+        t = t[:, 1:]  # exclude background
         t = Variable(t).cuda()  # [N,20]
-        
-        p = x.sigmoid()
-        pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
-        w = self.alpha*t + (1-self.alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
-        w = w * (1-pt).pow(self.gamma)
-        return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
 
-    def focal_loss_alt(self, x, y):
-        '''Focal loss alternative.
+        logit = F.softmax(x)
+        logit = logit.clamp(1e-7, 1.-1e-7)
+        conf_loss_tmp = -1 * t.float() * torch.log(logit)
+        conf_loss_tmp = alpha * conf_loss_tmp * (1-logit)**gamma
+        conf_loss = conf_loss_tmp.sum()
 
-        Args:
-          x: (tensor) sized [N,D].
-          y: (tensor) sized [N,].
+        return conf_loss
 
-        Return:
-          (tensor) focal loss.
-        '''
-        t = one_hot_embedding(y.data.cpu(), 1+self.classes)
-        t = t[:,1:]
-        t = Variable(t).cuda()
-
-        xt = x*(2*t-1)  # xt = x if t > 0 else -x
-        pt = (2*xt+1).sigmoid()
-
-        w = self.alpha*t + (1-self.alpha)*(1-t)
-        loss = -w*pt.log() / 2
-        return loss.sum()
-        
     def forward(self, loc_preds, loc_targets, cls_preds, cls_targets):
-        '''Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
-
+        """Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
         Args:
-          loc_preds: (tensor) predicted locations, sized [batch_size, #anchors, 4].
-          loc_targets: (tensor) encoded target locations, sized [batch_size, #anchors, 4].
-          cls_preds: (tensor) predicted class confidences, sized [batch_size, #anchors, #classes].
-          cls_targets: (tensor) encoded target labels, sized [batch_size, #anchors].
-
-        loss:
+          loc_preds(tensor): predicted locations, sized [batch_size, #anchors, 4].
+          loc_targets(tensor): encoded target locations, sized [batch_size, #anchors, 4].
+          cls_preds(tensor): predicted class confidences, sized [batch_size, #anchors, #classes].
+          cls_targets(tensor): encoded target labels, sized [batch_size, #anchors].
+        Returns:
           (tensor) loss = SmoothL1Loss(loc_preds, loc_targets) + FocalLoss(cls_preds, cls_targets).
-        '''
-        batch_size, num_boxes = cls_targets.size()
+        """
+
         pos = cls_targets > 0  # [N,#anchors]
         num_pos = pos.data.long().sum()
 
         ################################################################
         # loc_loss = SmoothL1Loss(pos_loc_preds, pos_loc_targets)
         ################################################################
-        mask = pos.unsqueeze(2).expand_as(loc_preds)       # [N,#anchors,4]
-        masked_loc_preds = loc_preds[mask].view(-1,4)      # [#pos,4]
-        masked_loc_targets = loc_targets[mask].view(-1,4)  # [#pos,4]
+        mask = pos.unsqueeze(2).expand_as(loc_preds)  # [N,#anchors,4]
+        masked_loc_preds = loc_preds[mask].view(-1, 4)  # [#pos,4]
+        masked_loc_targets = loc_targets[mask].view(-1, 4)  # [#pos,4]
         loc_loss = F.smooth_l1_loss(masked_loc_preds, masked_loc_targets, size_average=False)
 
         ################################################################
@@ -88,8 +77,8 @@ class FocalLoss(nn.Module):
         pos_neg = cls_targets > -1  # exclude ignored anchors
         mask = pos_neg.unsqueeze(2).expand_as(cls_preds)
         masked_cls_preds = cls_preds[mask].view(-1, self.classes)
-        cls_loss = self.focal_loss_alt(masked_cls_preds, cls_targets[pos_neg])
-
-        # print('loc_loss: %.3f | cls_loss: %.3f' % (loc_loss.data/num_pos, cls_loss.data/num_pos), end=' | ')
+        cls_loss = self.focal_loss(masked_cls_preds, cls_targets[pos_neg])
+        num_pos = max(1.0, num_pos.item())
+        
         loss = (loc_loss+cls_loss)/num_pos
         return loss, loc_loss.data/num_pos, cls_loss.data/num_pos
